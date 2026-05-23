daea61117f37c158555d4279a56a8e59b484c36f
Piyusha <pxsha23@gmail.com>
2026-05-23 18:04:21 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:8000";
+const url = process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
 
 export const addUser = async (data) => {
   try {
